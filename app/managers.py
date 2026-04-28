c636f8a4cc999ebb44af6a046c6dc1ff3b6d0fc3
zwirok-git <oleksijprokopenko@gmail.com>
2026-04-28 18:29:01 +0300

    implemented model & manager

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,59 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(
+            self,
+            db_name: str,
+            table_name: str
+    ) -> None:
+        self.table_name = table_name
+        self._connection = sqlite3.connect(db_name)
+
+    def create(
+            self,
+            first_name: str,
+            last_name: str
+    ) -> None:
+        self._connection.execute(
+            f"""
+            INSERT INTO {self.table_name} (first_name, last_name)
+            VALUES (?, ?)
+            """,
+            (first_name, last_name)
+        )
+        self._connection.commit()
+
+    def all(self) -> list:
+        cursor = self._connection.execute(
+            f"SELECT * FROM {self.table_name}"
+        )
+
+        return [Actor(*row) for row in cursor]
+
+    def update(
+            self,
+            pk: int,
+            new_first_name: str,
+            new_last_name: str
+    ) -> None:
+        self._connection.execute(
+            f"""
+            UPDATE {self.table_name}
+            SET first_name = ?, last_name = ?
+            WHERE id = ?
+            """,
+            (
+                new_first_name,
+                new_last_name,
+                pk
+            )
+        )
+        self._connection.commit()
+
+    def delete(self, pk: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ?",
+            (pk,)
+        )
+        self._connection.commit()
--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass
 
 
-# add dataclass here
+@dataclass
+class Actor:
+    id: int
+    first_name: str
+    last_name: str
